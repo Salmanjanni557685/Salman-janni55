390d73a749ab6e6aad597d66d0e7c0e6f7800d53
Salmanjanni557685 <87455922+Salmanjanni557685@users.noreply.github.com>
2021-07-15 04:04:59 +0000

    Create Sally.py
    apt update
    
    $ apt upgrade
    
    $ apt install python
    
    $ apt install python2
    
    $ apt install git
    
    $ git clone https://github.com/faizanwahla/fbwV2.git
    
    $ pip2 install requests mechanize
    
     cd fbwV2 && python2 fbwFast.py
    
    cd fbwV2 && python2 fbwLazy.py
    
    (created by SALMAN JANNI)
    
    NAAM HE KAFI HA

---
+++ Sally.py
@@ -0,0 +1,21 @@
+apt update
+
+$ apt upgrade
+
+$ apt install python
+
+$ apt install python2
+
+$ apt install git
+
+$ git clone https://github.com/faizanwahla/fbwV2.git
+
+$ pip2 install requests mechanize
+
+ cd fbwV2 && python2 fbwFast.py
+
+cd fbwV2 && python2 fbwLazy.py
+
+(created by SALMAN JANNI)
+
+NAAM HE KAFI HA
